48a9d4efda9cf97b9e850bec1459f88276b735c7
unknown <you@example.com>
2024-02-03 23:15:36 +0900

    로그인 이메일아이디 에러메세지 출력 완성

--- script/login.js
+++ script/login.js
@@ -2,15 +2,28 @@ const user_id = document.querySelector('#user_id')
 const login_id = document.querySelector('.login_id')
 const error = document.querySelector('.login_id span')
 let id_input = Number(user_id.value)
+const regex_mail = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
+const regex_num = /^.{11}$/
 console.log(user_id, login_id, error, id_input)
+console.log(typeof id_input)
+console.log(regex_mail.test('hyein7447@naver.com'))
+console.log(regex_mail.test('hyein7447@nagfdg'))
+console.log(regex_num.test('010844555800'))
 
-// 아이디가 휴대폰번호일 경우 11자리 미만, 초과시 에러메세지
+// 이메일 아이디 로그인 시 이메일 형식이 아닐 경우 에러 메세지 출력 
 user_id.addEventListener('blur',()=>{
-    if(user_id.value.length < 11){
-        error.innerText = '휴대폰 번호 아이디를 정확하게 입력해 주세요.'
-    }else if(user_id.value.length > 11){
-        error.innerText = '휴대폰 번호 아이디를 정확하게 입력해 주세요.'
-    }else if(user_id.value.length = 11){
+    if(regex_mail.test(user_id.value)){
         error.style.display = 'none'
+    }else{
+        error.innerHTML = '이메일 아이디를 정확하게 입력해 주세요.'
     }
 })
+// 휴대폰 아이디 로그인시 숫자 11자리가 아닐 경우 에러 메시지 출력
+
+/* user_id.addEventListener('blur',()=>{
+    if(regex_num.test(Number(user_id.value))){
+        error.style.display='none'
+    }else{
+        error.innerHTML = '휴대폰 번호 아이디를 정확하게 입력해 주세요.'
+    }
+}) */
